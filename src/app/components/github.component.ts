851fb1780627fbaac72210aa2754ead841e31f9f
pradyot <pradyotprksh4@gmail.com>
2018-08-29 16:34:49 +0530

    Changes

--- src/app/components/github.component.ts
+++ src/app/components/github.component.ts
@@ -2,29 +2,28 @@ import { Component } from '@angular/core';
 import {GithubService} from '../services/github.service';
 
 @Component({
-  moduleId:module.id,
+  moduleId: module.id,
   selector: 'github',
   templateUrl: 'github.component.html',
-  providers:[GithubService]
+  providers: [GithubService]
 })
 export class GithubComponent {
-  user:any;
-  repos:any;
-  username:string;
+  user: any;
+  repos: any;
+  username: string;
 
-  constructor(private _githubService:GithubService){
+  constructor(private _githubService: GithubService) {
     console.log('Github Component Init...');
   }
 
-  search(){
+  search() {
     this._githubService.updateUsername(this.username);
 
-    this._githubService.getUser().subscribe(user => {
-      //console.log(user);
+    this._githubService.getUser().subscribe( user => {
       this.user = user;
     });
 
-    this._githubService.getRepos().subscribe(repos => {
+    this._githubService.getRepos().subscribe( repos => {
       this.repos = repos;
     });
   }
